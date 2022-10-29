2d772b3bd6ab3b69e613349fdba37cd21f751c69
alyamafo <smokkb@mail.ru>
2022-10-29 15:33:52 +0500

    scroll

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 const btn = document.querySelector('.menu-btn');
 const nav = document.querySelector('.nav');
+const newBtn = document.querySelector('.new-btn-link');
+const newBlock = document.querySelector('.new')
+function handleButtonClick() {
+    newBlock.scrollIntoView({block: "center", behavior: "smooth"});
+ }
+ 
+ newBtn.addEventListener('click', handleButtonClick);
 
+ 
 btn.addEventListener('click', ()=>{
    nav.classList.toggle('menu-open');
 })
+
+
 
