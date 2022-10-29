881989b310c212cd74967c4c3f97bb94c2e56fec
alyamafo <smokkb@mail.ru>
2022-10-29 16:04:27 +0500

    scroll

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 const btn = document.querySelector('.menu-btn');
 const nav = document.querySelector('.nav');
-const newBtn = document.querySelector('.new-btn-link');
-const newBlock = document.querySelector('.new')
-function handleButtonClick() {
-    newBlock.scrollIntoView({block: "center", behavior: "smooth"});
+const btnNew= document.querySelector('.new-btn-link');
+const newBlock = document.querySelector('.new');
+const btnColor = document.querySelector('.btn-color');
+const colorBlock = document.querySelector('.color');
+const btnBuy = document.querySelector('.btn-buy');
+const contactsBlock = document.querySelector('.contacts');
+
+function handleButtonClick(e) {
+    const arrBlock =[newBlock, colorBlock, contactsBlock]
+   const id =e.target.id
+    arrBlock[id].scrollIntoView({block: "center", behavior: "smooth"});
  }
- 
- newBtn.addEventListener('click', handleButtonClick);
+
+ btnNew.addEventListener('click', handleButtonClick);
+ btnColor.addEventListener('click', handleButtonClick);
+ btnBuy.addEventListener('click', handleButtonClick);
+
 
  
 btn.addEventListener('click', ()=>{
